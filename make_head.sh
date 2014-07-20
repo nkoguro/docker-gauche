9e3adcff45796dd1c67267431d65b4da2e9edd4d
KOGURO, Naoki <naoki@koguro.net>
2014-07-20 18:55:29 +0900

    Do not use cache in build.

--- make_head.sh
+++ make_head.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t nkoguro/gauche head
+docker build --no-cache -t nkoguro/gauche head
 docker tag nkoguro/gauche nkoguro/gauche:head
--- make_stable.sh
+++ make_stable.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t nkoguro/gauche:stable stable
+docker build --no-cache -t nkoguro/gauche:stable stable
 docker tag nkoguro/gauche:stable nkoguro/gauche:0.9.4
